07f6eaafcef78be43bfc94179919b644b1e95998
Adrian.Grullon <Adrian.Grullon@GSS.local>
2023-10-18 14:04:42 -0400

    Proyecto Web Api Usuario

---
+++ WebAPI/Data/UsuarioData.cs
@@ -0,0 +1,173 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using WebAPI.Models;
+
+namespace WebAPI.Data
+{
+    public class UsuarioData
+    {
+        public static bool Registrar(Usuario oUsuario)
+        {
+            using (SqlConnection oConexion = new SqlConnection(Conexion.rutaConexion))
+            {
+                SqlCommand cmd = new SqlCommand("usp_registrar", oConexion);
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.Parameters.AddWithValue("@nombre", oUsuario.nombre);
+                cmd.Parameters.AddWithValue("@email", oUsuario.email);
+                cmd.Parameters.AddWithValue("@telefono", oUsuario.telefono);
+                cmd.Parameters.AddWithValue("@fecha_nacimiento", oUsuario.fecha_nacimiento);
+                cmd.Parameters.AddWithValue("@sexo", oUsuario.sexo);
+                cmd.Parameters.AddWithValue("@contrasena", oUsuario.contrasena);
+
+                try
+                {
+                    oConexion.Open();
+                    cmd.ExecuteNonQuery();
+                    return true;
+                }
+                catch (Exception ex)
+                {
+                    return false;
+                }
+            }
+        }
+
+        public static bool Modificar(Usuario oUsuario)
+        {
+            using (SqlConnection oConexion = new SqlConnection(Conexion.rutaConexion))
+            {
+                SqlCommand cmd = new SqlCommand("usp_modificar", oConexion);
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.Parameters.AddWithValue("@id_usuario", oUsuario.id_usuario);
+                cmd.Parameters.AddWithValue("@nombre", oUsuario.nombre);
+                cmd.Parameters.AddWithValue("@email", oUsuario.email);
+                cmd.Parameters.AddWithValue("@telefono", oUsuario.telefono);
+                cmd.Parameters.AddWithValue("@fecha_nacimiento", oUsuario.fecha_nacimiento);
+                cmd.Parameters.AddWithValue("@sexo", oUsuario.sexo);
+                cmd.Parameters.AddWithValue("@contrasena", oUsuario.contrasena);
+                try
+                {
+                    oConexion.Open();
+                    cmd.ExecuteNonQuery();
+                    return true;
+                }
+                catch (Exception ex)
+                {
+                    return false;
+                }
+            }
+        }
+
+        public static List<Usuario> Listar()
+        {
+            List<Usuario> oListaUsuario = new List<Usuario>();
+            using (SqlConnection oConexion = new SqlConnection(Conexion.rutaConexion))
+            {
+                SqlCommand cmd = new SqlCommand("usp_listar", oConexion);
+                cmd.CommandType = CommandType.StoredProcedure;
+
+                try
+                {
+                    oConexion.Open();
+                    cmd.ExecuteNonQuery();
+
+                    using (SqlDataReader dr = cmd.ExecuteReader())
+                    {
+
+                        while (dr.Read())
+                        {
+                            oListaUsuario.Add(new Usuario()
+                            {
+                                id_usuario = Convert.ToInt32(dr["id_usuario"]),
+                                nombre = dr["nombre"].ToString(),
+                                email = dr["email"].ToString(),
+                                telefono = dr["telefono"].ToString(),
+                                fecha_nacimiento = Convert.ToDateTime(dr["fecha_nacimiento"].ToString()),
+                                sexo = dr["sexo"].ToString(),
+                                contrasena = dr["contrasena"].ToString(),
+                            });
+                        }
+
+                    }
+
+
+
+                    return oListaUsuario;
+                }
+                catch (Exception ex)
+                {
+                    return oListaUsuario;
+                }
+            }
+        }
+
+        public static Usuario Obtener(int idusuario)
+        {
+            Usuario oUsuario = new Usuario();
+            using (SqlConnection oConexion = new SqlConnection(Conexion.rutaConexion))
+            {
+                SqlCommand cmd = new SqlCommand("usp_obtener", oConexion);
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.Parameters.AddWithValue("@idusuario", idusuario);
+
+                try
+                {
+                    oConexion.Open();
+                    cmd.ExecuteNonQuery();
+
+                    using (SqlDataReader dr = cmd.ExecuteReader())
+                    {
+
+                        while (dr.Read())
+                        {
+                            oUsuario = new Usuario()
+                            {
+                                id_usuario = Convert.ToInt32(dr["id_usuario"]),
+                                nombre = dr["nombre"].ToString(),
+                                email = dr["email"].ToString(),
+                                telefono = dr["telefono"].ToString(),
+                                fecha_nacimiento = Convert.ToDateTime(dr["fecha_nacimiento"].ToString()),
+                                sexo = dr["sexo"].ToString(),
+                                contrasena = dr["contrasena"].ToString()
+                            };
+                        }
+
+                    }
+
+
+
+                    return oUsuario;
+                }
+                catch (Exception ex)
+                {
+                    return oUsuario;
+                }
+            }
+        }
+
+        public static bool Eliminar(int id)
+        {
+            using (SqlConnection oConexion = new SqlConnection(Conexion.rutaConexion))
+            {
+                SqlCommand cmd = new SqlCommand("usp_eliminar", oConexion);
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.Parameters.AddWithValue("@idusuario", id);
+
+                try
+                {
+                    oConexion.Open();
+                    cmd.ExecuteNonQuery();
+                    return true;
+                }
+                catch (Exception ex)
+                {
+                    return false;
+                }
+            }
+        }
+    }
+}
---
+++ WebAPI/Models/Usuario.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebAPI.Models
+{
+    public class Usuario
+    {
+        public int id_usuario { get; set; }
+        public string nombre { get; set; }
+        public string email { get; set; }
+        public string telefono { get; set; }
+        public DateTime fecha_nacimiento { get; set; }
+        public string sexo { get; set; }
+        public string contrasena { get; set; }
+    }
+}
